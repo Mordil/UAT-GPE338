859a41bf785372f3529e3601fd46ea0fd53f439a
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-10 15:25:28 -0700

    Update and Move StoreItemModel
    - Change: StoreItem name to StoreItemModel
    - Remove: Lock object usage

---
+++ Harris.Nathan_Week1/Assets/Scripts/Models/StoreItemModel.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    [Serializable]
+    public class StoreItemModel : IJsonModel
+    {
+        public delegate void ImageDownloadedHandler();
+
+        public ImageDownloadedHandler ImageDownloaded;
+
+        public string title;
+        public string thumbnailUrl;
+        public Texture2D thumbnailImage;
+
+        public static StoreItemModel CreateFromJSON(string json)
+        {
+            return JsonUtility.FromJson<StoreItemModel>(json);
+        }
+
+        public IEnumerator FetchImageData()
+        {
+            WWW request = new WWW(thumbnailUrl + ".png");
+
+            yield return request;
+
+            thumbnailImage = request.texture;
+
+            ImageDownloaded();
+        }
+    }
+}
