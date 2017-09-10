828502b970f1629a04c713dc966cb8792420543c
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-10 15:25:51 -0700

    Add StoreItem Component

---
+++ Harris.Nathan_Week1/Assets/Scripts/UI/Store/StoreItem.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Interfaces;
+using Assets.Scripts.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreItem : MonoBehaviour, IModelBacked<StoreItemModel>
+{
+    public StoreItemModel Model { get; set; }
+
+    [SerializeField]
+    private Text ItemTitle;
+    [SerializeField]
+    private RawImage ItemImage;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        StoreItemModel.ImageDownloadedHandler assignment = () => ItemImage.texture = Model.thumbnailImage;
+
+        ItemTitle.text = Model.title;
+        
+        if (Model.thumbnailImage == null)
+        {
+            Model.ImageDownloaded += () =>
+            {
+                assignment();
+                Model.ImageDownloaded -= assignment;
+            };
+            StartCoroutine(Model.FetchImageData());
+        }
+        else
+        {
+            ItemImage.texture = Model.thumbnailImage;
+        }
+    }
+}
