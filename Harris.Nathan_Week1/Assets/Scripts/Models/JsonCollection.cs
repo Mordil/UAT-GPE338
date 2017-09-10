ceff0064157bec3b8fd8034da73ccc086169d684
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-10 15:23:27 -0700

    Add JsonCollection Generic Class
    - Add: JsonCollection for wrapping JSON with no root node with a root node or parsing sub-objects with Unity's Json Utility

---
+++ Harris.Nathan_Week1/Assets/Scripts/Models/JsonCollection.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Interfaces;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    public class JsonCollection<T>
+        where T : IJsonModel
+    {
+        public T[] items;
+
+        public static JsonCollection<T> CreateFromJSON(string json, int maxItems = 0, bool wrapRootNode = false)
+        {
+            if (wrapRootNode)
+            {
+                json = "{ \"items\": " + json + " }";
+            }
+            return CreateFromJSON(json, maxItems);
+        }
+
+        private static JsonCollection<T> CreateFromJSON(string json, int maxItems)
+        {
+            var result = JsonUtility.FromJson<JsonCollection<T>>(json);
+            
+            if (maxItems > 0)
+            {
+                result.items = result.items
+                    .Take(maxItems)
+                    .ToArray();
+            }
+
+            return result;
+        }
+    }
+}
