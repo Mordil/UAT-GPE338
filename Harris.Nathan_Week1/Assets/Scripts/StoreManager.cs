8aa641041d831be89d87f2f410e02c2cf49b7401
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-09 20:50:13 -0700

    Add StoreManager Class
    - Add: StoreManager Class for making URL requests with stub for transforming the JSON data

---
+++ Harris.Nathan_Week1/Assets/Scripts/StoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StoreManager : MonoBehaviour
+{
+    public UnityEvent OnMakingRequest;
+    public UnityEvent OnStoreIsReady;
+
+    [SerializeField]
+    private GameObject _storeContentContainer;
+
+    public void FetchStoreData()
+    {
+        StartCoroutine(MakeUrlRequest());
+        OnMakingRequest.Invoke();
+    }
+
+    private IEnumerator MakeUrlRequest()
+    {
+        var request = new WWW("https://jsonplaceholder.typicode.com/photos");
+        
+        yield return request;
+        
+        TransformRequestJSON(request.text);
+    }
+
+    private void TransformRequestJSON(string json)
+    {
+        // TODO: StoreItem factory
+        Debug.Log(json);
+    }
+}
