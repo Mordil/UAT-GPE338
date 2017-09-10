38f45027f0f17bfd84532eb53daee8444123135f
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-09 21:17:35 -0700

    Add Assignment Scene
    - Add: Week1 scene with views for different request stages
    - Add: Rotation script for quick rotation animations

---
+++ Harris.Nathan_Week1/Assets/Scripts/Animation/RotationAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RotationAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 5.0f;
+    [SerializeField]
+    private Transform _myTransform;
+
+    private void Awake()
+    {
+        if (_myTransform == null)
+        {
+            _myTransform = transform;
+        }
+    }
+
+    private void Update()
+    {
+        float zValue = _myTransform.localEulerAngles.z + Time.deltaTime * _rotationSpeed;
+        _myTransform.rotation = Quaternion.Euler(0, 0, zValue);
+    }
+}
